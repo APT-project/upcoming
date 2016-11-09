91d25cbc81b5ab542de107d8a113b626c0bba843
Antti <github.TIEA4@dfgh.net>
2016-11-09 03:29:32 +0200

    Fix indentation

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -7,28 +7,28 @@ angular
   .module('upcomingStops')
   .run(['$rootScope', function($rootScope) {
 
-  $rootScope.fullscreen = false;
-  $rootScope.initialized = false;
+    $rootScope.fullscreen = false;
+    $rootScope.initialized = false;
 
-  llb_app.addListener('window_state', function(data){
-    if(data.fullscreen) {
+    llb_app.addListener('window_state', function(data){
+      if(data.fullscreen) {
+        $rootScope.$apply(function() {
+          $rootScope.fullscreen = true
+        })
+      } else {
+        $rootScope.$apply(function() {
+          $rootScope.fullscreen = false
+          $rootScope.$broadcast("changed_window_state");
+        })
+      }
+    });
+
+    llb_app.request('window_dimensions');
+
+    llb_app.addListener('window_dimensions', function(data) {
       $rootScope.$apply(function() {
-        $rootScope.fullscreen = true
+        $rootScope.window_dimensions = data
+        $rootScope.initialized = true;
       })
-    } else {
-      $rootScope.$apply(function() {
-        $rootScope.fullscreen = false
-        $rootScope.$broadcast("changed_window_state");
-      })
-    }
-  });
-
-  llb_app.request('window_dimensions');
-
-  llb_app.addListener('window_dimensions', function(data) {
-    $rootScope.$apply(function() {
-      $rootScope.window_dimensions = data
-      $rootScope.initialized = true;
-    })
-  });
-}]);
+    });
+  }]);
