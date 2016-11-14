8931d09bae8d92eeada80420c270ab4b1f33281d
Antti <github.TIEA4@dfgh.net>
2016-11-14 14:58:11 +0200

    Change UI
     - Use `md-card`s instead of `md-button`s in the event list
     - Use `md-sidenav` so there's no need for another navigation
       level
     - Use `md-bottom-sheet` to display event details in the map

--- app/js/controllers/event.controller.js
+++ app/js/controllers/event.controller.js
@@ -5,17 +5,45 @@
     .module('upcomingStops')
     .controller('EventController', EventController);
 
-  EventController.$inject = ['eventFactory'];
+  EventController.$inject = ['eventFactory', '$mdSidenav', '$mdBottomSheet'];
 
-  function EventController(eventFactory) {
-
+  function EventController(eventFactory, $mdSidenav, $mdBottomSheet) {
     let vm = this;
 
-    eventFactory.getEvents().then(function success(response) {
-      vm.events = response.data;
-    }, function error(response) {
-      console.log(response.status + response.statusText);
-    });
+    vm.events = [];
+    vm.selectedEvent = null;
+    vm.selectEvent = selectEvent;
+    vm.toggleEventList = toggleEventList;
+    vm.openBottomSheet = openBottomSheet;
 
+    function selectEvent(event) {
+      vm.selectedEvent = event;
+    }
+
+    function toggleEventList() {
+      $mdSidenav('left').toggle();
+    }
+
+    function openBottomSheet(event) {
+      $mdBottomSheet.show({
+        //template: '<md-bottom-sheet>{{bs.event.name}}</md-bottom-sheet>',
+        templateUrl: 'templates/event.bottomsheet.html',
+        /* Wish I could just pass the parameter 'event' to the controller instead of referencing EventController's scope here. Waiting for someone to show me how to do it. Until then using this way.*/
+        controller: function() {this.event = vm.selectedEvent},
+        //controller: function(event) {this.event = event},
+        controllerAs: 'bs',
+        //locals: event
+      });
+    }
+
+    eventFactory
+      .getEvents()
+      .then(function success(response) {
+        vm.events = response.data;
+      },
+      function error(response) {
+        console.log(response.status + response.statusText);
+      });
   };
+
 })();
