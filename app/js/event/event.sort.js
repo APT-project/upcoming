a9178e3e4120953be1ee9e2800ecee8b8dc49134
Xiaodong Ming <m13996284930@gmail.com>
2016-12-03 22:58:06 +0200

    updated version of sort_envents

--- app/js/event/event.sort.js
+++ app/js/event/event.sort.js
@@ -7,53 +7,45 @@
 
   eventSort.$inject = ['$http'];
 
-  // function eventSort($http) {
-  //
-  //     var getEvents = function() {
-  //         return $http.get('data/mock.events.json');
-  //     };
-  //
-  //     return {
-  //         getEvents: getEvents
-  //     };
-  //
-  // };
+  function eventSort($http) {
 
-  function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1); // deg2rad below
-    var dLon = deg2rad(lon2-lon1);
-    var a =
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon/2) * Math.sin(dLon/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in km
-    return d;
-  };
+    var vm = this;
 
-  function deg2rad(deg) {
-    return deg * (Math.PI/180)
-  };
-
-  llb_app.addListener('location', function(data){
-    var latLng = {
-      lat: data.data.latitude,
-      lng: data.data.longitude
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1); // deg2rad below
+      var dLon = deg2rad(lon2-lon1);
+      var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var d = R * c; // Distance in km
+      return d;
     };
-  });
-  llb_app.request('location');
 
-  var distance_list = new Array();
-  function proximity(userCoordinates, $http) {
-    var events = $http.get('data/mock.events.json')
-    for(var i = 0; i < events.length; i++) {
-      // distance_list.push(events[i].name + " " + getDistanceFromLatLonInKm(Number(events[i].latitude), Number(events[i].longitude), Number(userCoordinates.latitude), Number(userCoordinates.longtitude)));
-      distance_list[getDistanceFromLatLonInKm(Number(events[i].latitude), Number(events[i].longitude), Number(userCoordinates.latitude), Number(userCoordinates.longtitude))] = events[i].name;
-    }
-    return distance_list;
+    function deg2rad(deg) {
+      return deg * (Math.PI/180)
+    };
+
+    llb_app.addListener('location', function(data){
+      var latLng = {
+        lat: data.data.latitude,
+        lng: data.data.longitude
+      };
+      vm.latestLocation = latLng;
+    });
+    llb_app.request('location');
+
+    function proximity(userCoordinates, $http) {
+      var events = $http.get('data/mock.events.json')
+      for(var i = 0; i < events.length; i++) {
+        // distance_list.push(events[i].name + " " + getDistanceFromLatLonInKm(Number(events[i].latitude), Number(events[i].longitude), Number(userCoordinates.latitude), Number(userCoordinates.longtitude)));
+        events[i].disanceToUser = getDistanceFromLatLonInKm(Number(events[i].latitude), Number(events[i].longitude), Number(vm.latestLocation.latitude), Number(vm.latestLocation.longtitude));
+      }
+      console.log(events);
+    };
   };
 
-  console.log(distance_list);
 
 })();
