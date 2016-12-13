90397fef19673af99df68a258d8fa7b1ec932516
Xiaodong Ming <m13996284930@gmail.com>
2016-12-13 02:02:20 +0200

    UI optimize and Code optimize

--- app/js/event/event.controller.js
+++ app/js/event/event.controller.js
@@ -5,21 +5,27 @@
     .module('upcomingStops.event')
     .controller('EventController', EventController);
 
-  EventController.$inject = ['eventFactory', 'eventSort', '$mdSidenav', '$mdBottomSheet'];
+  EventController.$inject = ['eventFactory', 'eventSort', '$mdSidenav', '$mdBottomSheet', '$scope'];
 
-  function EventController(eventFactory, eventSort, $mdSidenav, $mdBottomSheet) {
-    var vm = this;
+  function EventController(eventFactory, eventSort, $mdSidenav, $mdBottomSheet, $scope) {
+    // var vm = this;
 
-    vm.events = [];
-    vm.selectedEvent = null;
-    vm.latestLocation = { latitude: 61.498180, longitude: 23.762195 };
+    $scope.vm = [];
 
-    vm.selectEvent = selectEvent;
-    vm.toggleEventList = toggleEventList;
-    vm.openBottomSheet = openBottomSheet;
+    $scope.vm.events = [];
+    $scope.vm.selectedEvent = null;
+    $scope.vm.latestLocation = { latitude: 61.498180, longitude: 23.762195 };
+
+    $scope.vm.selectEvent = selectEvent;
+    $scope.vm.toggleEventList = toggleEventList;
+    $scope.vm.openBottomSheet = openBottomSheet;
+
+    $scope.$watch('vm',function(newValue, oldValue){
+      $scope.$apply();
+    });
 
     function selectEvent(event) {
-      vm.selectedEvent = event;
+      $scope.vm.selectedEvent = event;
     }
 
     function toggleEventList() {
@@ -31,7 +37,7 @@
         //template: '<md-bottom-sheet>{{bs.event.name}}</md-bottom-sheet>',
         templateUrl: 'templates/event.bottomsheet.html',
         /* Wish I could just pass the parameter 'event' to the controller instead of referencing EventController's scope here. Waiting for someone to show me how to do it. Until then using this way.*/
-        controller: function() {this.event = vm.selectedEvent},
+        controller: function() {this.event = $scope.vm.selectedEvent},
         //controller: function(event) {this.event = event},
         controllerAs: 'bs',
         //locals: event
@@ -44,16 +50,16 @@
         latitude: data.data.latitude,
         longitude: data.data.longitude
       };
-      vm.latestLocation = coordinates;
+      $scope.vm.latestLocation = coordinates;
 
       eventSort
-        .proximity(vm.latestLocation, vm.events);
+        .proximity($scope.vm.latestLocation, $scope.vm.events);
     });
 
     eventFactory
       .getEvents()
       .then(function success(response) {
-        vm.events = response.data;
+          $scope.vm.events = response.data;
       },
       function error(response) {
         console.log(response.status + response.statusText);
