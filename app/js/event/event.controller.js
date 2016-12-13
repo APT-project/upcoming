ae04fb365a1cddf08cfaff35891bc09ff0e14ba6
Xiaodong Ming <m13996284930@gmail.com>
2016-12-14 01:06:44 +0200

    remove  and change the tile stlye to the orginal one

--- app/js/event/event.controller.js
+++ app/js/event/event.controller.js
@@ -5,27 +5,21 @@
     .module('upcomingStops.event')
     .controller('EventController', EventController);
 
-  EventController.$inject = ['eventFactory', 'eventSort', '$mdSidenav', '$mdBottomSheet', '$scope'];
+  EventController.$inject = ['eventFactory', 'eventSort', '$mdSidenav', '$mdBottomSheet'];
 
-  function EventController(eventFactory, eventSort, $mdSidenav, $mdBottomSheet, $scope) {
-    // var vm = this;
+  function EventController(eventFactory, eventSort, $mdSidenav, $mdBottomSheet) {
+    var vm = this;
 
-    $scope.vm = [];
+    vm.events = [];
+    vm.selectedEvent = null;
+    vm.latestLocation = { latitude: 61.498180, longitude: 23.762195 };
 
-    $scope.vm.events = [];
-    $scope.vm.selectedEvent = null;
-    $scope.vm.latestLocation = { latitude: 61.498180, longitude: 23.762195 };
-
-    $scope.vm.selectEvent = selectEvent;
-    $scope.vm.toggleEventList = toggleEventList;
-    $scope.vm.openBottomSheet = openBottomSheet;
-
-    $scope.$watch('vm',function(newValue, oldValue){
-      $scope.$apply();
-    });
+    vm.selectEvent = selectEvent;
+    vm.toggleEventList = toggleEventList;
+    vm.openBottomSheet = openBottomSheet;
 
     function selectEvent(event) {
-      $scope.vm.selectedEvent = event;
+      vm.selectedEvent = event;
     }
 
     function toggleEventList() {
@@ -37,7 +31,7 @@
         //template: '<md-bottom-sheet>{{bs.event.name}}</md-bottom-sheet>',
         templateUrl: 'templates/event.bottomsheet.html',
         /* Wish I could just pass the parameter 'event' to the controller instead of referencing EventController's scope here. Waiting for someone to show me how to do it. Until then using this way.*/
-        controller: function() {this.event = $scope.vm.selectedEvent},
+        controller: function() {this.event = vm.selectedEvent},
         //controller: function(event) {this.event = event},
         controllerAs: 'bs',
         //locals: event
@@ -50,16 +44,16 @@
         latitude: data.data.latitude,
         longitude: data.data.longitude
       };
-      $scope.vm.latestLocation = coordinates;
+      vm.latestLocation = coordinates;
 
       eventSort
-        .proximity($scope.vm.latestLocation, $scope.vm.events);
+        .proximity(vm.latestLocation, vm.events);
     });
 
     eventFactory
       .getEvents()
       .then(function success(response) {
-          $scope.vm.events = response.data;
+          vm.events = response.data;
       },
       function error(response) {
         console.log(response.status + response.statusText);
