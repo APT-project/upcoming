7a9a2837bfc12ad53beb6697a58e01dc14d3327e
Antti <github.TIEA4@dfgh.net>
2017-01-08 16:18:32 +0200

    Make event list show meters again
    And refactor some related code

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -36,15 +36,6 @@
         $rootScope.$apply(function() {
           $rootScope.window_dimensions = data;
           $rootScope.initialized = true;
-
-          // Hide some details (distance), from the left navigation event list, if
-          // the screen is small.
-          // This doesn't trigger when the view is chaged to portrait/landscape mode
-          if (data.fullscreen_width < 321) {
-            $rootScope.smallEventDetails = true;
-          } else {
-            $rootScope.smallEventDetails = false;
-          }
         });
       });
     }]);
